61ee4657f53d257abb6f2916517f43984645dccf
MysteriousRob <robertjohngasior@gmail.com>
2024-07-24 16:36:03 +0200

    made sliders reset on refresh

--- src/components/Slider/sliders.jsx
+++ src/components/Slider/sliders.jsx
@@ -5,70 +5,70 @@ import { FaArrowCircleDown, FaArrowCircleUp } from "react-icons/fa";
 //import { sliderData } from "./sliderdata";
 
 function MultiVariableSlider() {
-  const [dropdownOpen, setDropdownOpen] = useState([false, false]);
+  const [dropdownOpen, setDropdownOpen] = useState([false, false, false, false, false, false]);
   const [sliderValues, setSliderValues] = useState([
     //event form 
     {title: 'Event Form', value: '0', 
         children:[
           {title: 'Event Average', value: '0'},
-          {title: 'Event-10', value: '1'},
-          {title: 'Event Wins', value: '2'},
-          {title: 'Last Year Tournament', value: '3'},
+          {title: 'Event-10', value: '0'},
+          {title: 'Event Wins', value: '0'},
+          {title: 'Last Year Tournament', value: '0'},
     ]},
 //current form
 // sliderValues[1].children[0].title 
 // sliderValues[1].children[1].title
-    {title: 'Current Form', value: '1',         
+    {title: 'Current Form', value: '0',         
       children:[
       {title: 'OWGR', value: '0'},     
-      {title: 'Last Tournament', value: '1'},
-      {title: 'Last but 1 Tournament', value: '2'},
-      {title: 'Last but 3 Tournament', value: '3'},
-      {title: 'Last but 5 Tournament', value: '4'},
+      {title: 'Last Tournament', value: '0'},
+      {title: 'Last but 1 Tournament', value: '0'},
+      {title: 'Last but 3 Tournament', value: '0'},
+      {title: 'Last but 5 Tournament', value: '0'},
 ]},
 //scoring stats
-    {title: 'Scoring Stats', value: '2',
+    {title: 'Scoring Stats', value: '0',
       children:[
       {title: 'Hole in One Average', value: '0'},
-      {title: 'Birdie Average', value: '1'},
-      {title: 'Eagle Average', value: '2'},
-      {title: 'Bogey Average', value: '3'},
-      {title: 'Par 3 Performance', value: '4'},
-      {title: 'Par 4 Performance', value: '5'},
-      {title: 'Par 5 Performance', value: '6'},
+      {title: 'Birdie Average', value: '0'},
+      {title: 'Eagle Average', value: '0'},
+      {title: 'Bogey Average', value: '0'},
+      {title: 'Par 3 Performance', value: '0'},
+      {title: 'Par 4 Performance', value: '0'},
+      {title: 'Par 5 Performance', value: '0'},
 ]},
 //Event Specific
 // sliderValues[3].children[0].title
-    {title: 'Event Specific', value: '3',
+    {title: 'Event Specific', value: '0',
     children:[
     {title: 'Par 70', value: '0'},
-    {title: 'Short Course', value: '1'},
-    {title: 'Bermuda Postive', value: '2'},
-    {title: 'Soft Ground', value: '3'},
-    {title: 'Fazio Design', value: '4'},
-    {title: 'Costal Course', value: '5'},
+    {title: 'Short Course', value: '0'},
+    {title: 'Bermuda Postive', value: '0'},
+    {title: 'Soft Ground', value: '0'},
+    {title: 'Fazio Design', value: '0'},
+    {title: 'Costal Course', value: '0'},
 ]},
 //Strokes Gained
-    {title: 'Strokes Gained', value: '4',
+    {title: 'Strokes Gained', value: '0',
     children:[
     {title: 'SG off the Tee', value: '0'},
-    {title: 'SG Approach', value: '1'},
-    {title: 'SG Tee to Green', value: '2'},
-    {title: 'SG arround the Green', value: '3'},
-    {title: 'SG Putting', value: '4'},
+    {title: 'SG Approach', value: '0'},
+    {title: 'SG Tee to Green', value: '0'},
+    {title: 'SG arround the Green', value: '0'},
+    {title: 'SG Putting', value: '0'},
 ]},
 //skill stats
-    {title: 'Skill Stats', value: '5',
+    {title: 'Skill Stats', value: '0',
     children:[
     {title: 'Driving Distance', value: '0'},
-    {title: 'Proximity to Hole', value: '1'},
-    {title: 'Going for Green', value: '2'},
-    {title: 'Driving Accuracy', value: '3'},
-    {title: 'GIR', value: '4'},
-    {title: 'Scrambling', value: '5'},
-    {title: 'Sand Saves', value: '6'},
-    {title: 'Putting Average', value: '7'},
-    {title: 'Puts per Round', value: '8'},
+    {title: 'Proximity to Hole', value: '0'},
+    {title: 'Going for Green', value: '0'},
+    {title: 'Driving Accuracy', value: '0'},
+    {title: 'GIR', value: '0'},
+    {title: 'Scrambling', value: '0'},
+    {title: 'Sand Saves', value: '0'},
+    {title: 'Putting Average', value: '0'},
+    {title: 'Puts per Round', value: '0'},
 ]},
   ]);
 
@@ -94,9 +94,9 @@ function MultiVariableSlider() {
 
   return (
     <div>
-      <div className="bg-black flex flex-col ml-[15%]">
+      <div className="flex flex-col p-5 ml-[15%]">
         {/* event form */}
-        <div tabIndex={0} className="bg-white relative text-black mb-5 pb-5">
+        <div tabIndex={0} className="relative rounded-lg bg-white text-black mb-5 px-3 py-[7px]">
           <div role="button" className="flex justify-center relative">
             <p className="text-black">Event Form</p>
             <Slider1 
@@ -111,7 +111,7 @@ function MultiVariableSlider() {
               <FaArrowCircleDown onClick={() => toggleDropdown(0)} />
             )}
           </div>
-          <ul className={`dropdown-content ${dropdownOpen[0] ? "show" : ""}`}>
+          <ul className={`dropdown-content ${dropdownOpen[0] ? "show" : "hidden"}`}>
             {sliderValues[0].children.map((child, idx) => (
               <li className="list-none p-0 m-0 block" key={idx}>
                 <p className="text-black">{child.title}</p>
@@ -129,7 +129,7 @@ function MultiVariableSlider() {
           </ul>
         </div>
        {/* current form */}
-       <div tabIndex={1} className="relative bg-white text-black mb-5">
+       <div tabIndex={1} className="relative rounded-lg bg-white text-black mb-5 px-3 py-[7px]">
           <div role="button" className="flex justify-center relative">
             <p className="text-black">Current Form</p>
             <Slider1 
@@ -144,7 +144,7 @@ function MultiVariableSlider() {
               <FaArrowCircleDown onClick={() => toggleDropdown(1)} />
             )}
           </div>
-          <ul className={`dropdown-content ${dropdownOpen[1] ? "show" : ""}`}>
+          <ul className={`dropdown-content ${dropdownOpen[1] ? "show" : "hidden"}`}>
             {sliderValues[1].children.map((child, idx) => (
               <li className="list-none p-0 m-0 block" key={idx}>
                 <p className="text-black">{child.title}</p>
@@ -162,7 +162,7 @@ function MultiVariableSlider() {
           </ul>
         </div>
        {/* Scoring stats */}
-       <div tabIndex={2} className="relative bg-white text-black mb-5 pb-5">
+       <div tabIndex={2} className="relative rounded-lg bg-white text-black mb-5 px-3 py-[7px]">
           <div role="button" className="flex justify-center relative">
             <p className="text-black">Scoring Stats</p>
             <Slider1 
@@ -177,7 +177,7 @@ function MultiVariableSlider() {
               <FaArrowCircleDown onClick={() => toggleDropdown(2)} />
             )}
           </div>
-          <ul className={`dropdown-content ${dropdownOpen[2] ? "show" : ""}`}>
+          <ul className={`dropdown-content ${dropdownOpen[2] ? "show" : "hidden"}`}>
             {sliderValues[2].children.map((child, idx) => (
               <li className="list-none p-0 m-0 block" key={idx}>
                 <p className="text-black">{child.title}</p>
@@ -187,7 +187,7 @@ function MultiVariableSlider() {
                   className="w-[100px]"
                   value={child.value}
                   setSliderValues={setSliderValues}
-                  parentTitle="scoring stats"
+                  parentTitle="Scoring Stats"
                   childTitle={child.title}
                 />
               </li>
@@ -195,7 +195,7 @@ function MultiVariableSlider() {
           </ul>
         </div>
        {/* event specific */}
-       <div tabIndex={3} className="relative bg-white text-black mb-5">
+       <div tabIndex={3} className="relative rounded-lg bg-white text-black mb-5 px-3 py-[7px]">
           <div role="button" className="flex justify-center relative">
             <p className="text-black">Event Specific</p>
             <Slider1 
@@ -210,7 +210,7 @@ function MultiVariableSlider() {
               <FaArrowCircleDown onClick={() => toggleDropdown(3)} />
             )}
           </div>
-          <ul className={`dropdown-content ${dropdownOpen[3] ? "show" : ""}`}>
+          <ul className={`dropdown-content ${dropdownOpen[3] ? "show" : "hidden"}`}>
             {sliderValues[3].children.map((child, idx) => (
               <li className="list-none p-0 m-0 block" key={idx}>
                 <p className="text-black">{child.title}</p>
@@ -228,7 +228,7 @@ function MultiVariableSlider() {
           </ul>
         </div>
        {/* Strokes Gained*/}
-       <div tabIndex={4} className="relative bg-white text-black mb-5">
+       <div tabIndex={4} className="relative rounded-lg bg-white text-black mb-5 px-3 py-[7px]">
           <div role="button" className="flex justify-center relative">
             <p className="text-black">Strokes Gained</p>
             <Slider1 
@@ -243,7 +243,7 @@ function MultiVariableSlider() {
               <FaArrowCircleDown onClick={() => toggleDropdown(4)} />
             )}
           </div>
-          <ul className={`dropdown-content ${dropdownOpen[4] ? "show" : ""}`}>
+          <ul className={`dropdown-content ${dropdownOpen[4] ? "show" : "hidden"}`}>
             {sliderValues[4].children.map((child, idx) => (
               <li className="list-none p-0 m-0 block" key={idx}>
                 <p className="text-black">{child.title}</p>
@@ -261,7 +261,7 @@ function MultiVariableSlider() {
           </ul>
         </div>
        {/* skill stats */}
-       <div tabIndex={5} className="relative bg-white text-black mb-5">
+       <div tabIndex={5} className="relative rounded-lg bg-white text-black mb-5 px-3 py-[7px]">
           <div role="button" className="flex justify-center relative">
             <p className="text-black">Skill Stats</p>
             <Slider1 
@@ -276,7 +276,7 @@ function MultiVariableSlider() {
               <FaArrowCircleDown onClick={() => toggleDropdown(5)} />
             )}
           </div>
-          <ul className={`dropdown-content ${dropdownOpen[5] ? "show" : ""}`}>
+          <ul className={`dropdown-content ${dropdownOpen[5] ? "show" : "hidden"}`}>
             {sliderValues[5].children.map((child, idx) => (
               <li className="list-none p-0 m-0 block" key={idx}>
                 <p className="text-black">{child.title}</p>
