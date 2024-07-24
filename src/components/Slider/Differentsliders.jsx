da705f7d57f4c76df19c42ecd814ff91cbf19063
MysteriousRob <robertjohngasior@gmail.com>
2024-07-24 11:59:10 +0200

    got rid of some unwanted spacing

--- src/components/Slider/Differentsliders.jsx
+++ src/components/Slider/Differentsliders.jsx
@@ -1,59 +1,50 @@
-import React from 'react';
+import React from "react";
 import "./../../assets/css/GBS.css";
 
-
-export function Slider1({parentTitle, childTitle, setSliderValues, max=10, value, parent}) {
-  return(
+export function Slider1({
+  parentTitle,
+  childTitle,
+  setSliderValues,
+  max = 10,
+  value,
+  parent,
+}) {
+  return (
     <>
-      <div className='slider-input1'>
-        <input 
-        onChange={(event) => {
-          setSliderValues(prev => {
-
-
-
-            if(parent)
-              {
-              return prev.map(slider => {
-                if(slider.title === parentTitle){
-                  slider.value = event.target.value
-                  slider.children.map(child => {
-                    child.value = event.target.value
-                  })
-                }
-                return slider
-              })}
-
-
-
-
-
-
-            return prev.map(slider => {
-              if(slider.title === parentTitle){
-                slider.children?.map(childSlider => {
-                  if(childSlider.title === childTitle){
-                    childSlider.value = event.target.value
+      <div className="slider-input1">
+        <input
+          onChange={(event) => {
+            setSliderValues((prev) => {
+              if (parent) {
+                return prev.map((slider) => {
+                  if (slider.title === parentTitle) {
+                    slider.value = event.target.value;
+                    slider.children.map((child) => {
+                      child.value = event.target.value;
+                    });
                   }
-                })
+                  return slider;
+                });
               }
-              return slider
-            }
-          
-          
-          
-          
-          )
-          })
-        }
-      }
-        type="range" 
-        min={0} 
-        max={max} 
-        value={value}
-        className="range range-primary" />
+              return prev.map((slider) => {
+                if (slider.title === parentTitle) {
+                  slider.children?.map((childSlider) => {
+                    if (childSlider.title === childTitle) {
+                      childSlider.value = event.target.value;
+                    }
+                  });
+                }
+                return slider;
+              });
+            });
+          }}
+          type="range"
+          min={0}
+          max={max}
+          value={value}
+          className="range range-primary"
+        />
       </div>
-      </>
-  )
+    </>
+  );
 }
-
