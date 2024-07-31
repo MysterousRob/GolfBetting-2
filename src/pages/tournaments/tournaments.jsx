7ef0e145ab9b1505d063f4658684df4e9282b841
MysteriousRob <robertjohngasior@gmail.com>
2024-07-31 13:28:46 +0200

    started working on the different tournaments for home page everything is working

--- src/pages/tournaments/tournaments.jsx
+++ src/pages/tournaments/tournaments.jsx
@@ -1,17 +1,14 @@
 import React from "react";
-import TheMasters2 from "./full info/themaster2";
-import TheOpen2 from "./full info/theopen2";
-import UsPga2 from "./full info/uspga2";
-import OpenChamp2 from "./full info/openchamp2";
+// import TheMasters2 from "./full info/themaster2";
+// import TheOpen2 from "./full info/theopen2";
+// import UsPga2 from "./full info/uspga2";
+// import OpenChamp2 from "./full info/openchamp2";
 
 
 function Competiton() {
   return (
     <div className="mt-[35px] p-5 text-[30px] text-white flex bg-transparent">
-      <UsPga2 />
-      <OpenChamp2 />
-      <TheMasters2 />
-      <TheOpen2 />
+      
     </div>
   );
 }
