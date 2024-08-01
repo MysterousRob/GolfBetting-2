5b140f0f693f072960f66b731dc82e11b056dd6d
MysteriousRob <robertjohngasior@gmail.com>
2024-08-01 13:49:30 +0200

    deleted everything, added base to the site

--- src/state/sitesdata.jsx
+++ src/state/sitesdata.jsx
@@ -1,131 +1,21 @@
 import React from "react";
 
-export const sectionData = [
-    {
-        sectionName: {
-            BonusData: "Bonus Data",
-            BonusCode: "Bonus Code",
-            BoostedOdds: "Boosted Odds",
-            LiveStreaming: "Live Streaming",
-            InPlayBetting: "In Play Betting",
-            TermsAndConditions: "T&C",
-        }
-    }, 
-]
+function SitesData() {
+    const [isMenuOpen, setIsMenuOpen] = useState(true);
 
-export const siteData = [
-    {
-       nameSite: "betway", 
-       info: {
-        bonusData: "",
-        bonusCode: "",
-        boostedOdds: "",
-        liveStreaming: "",
-        inPlayBetting: "",
-        termsCondidtions: "",
-       },
-       link: {
-        claimLink: "",
-        visitLink: "",
-        reviewLink: "",
-        },
-    },
-    {
-       nameSite: "bwin", 
-       info: {
-        bonusData: "",
-        bonusCode: "",
-        boostedOdds: "",
-        liveStreaming: "",
-        inPlayBetting: "",
-        termsCondidtions: "",
-       },
-       link: {
-        claimLink: "",
-        visitLink: "",
-        reviewLink: "",
-        },
-    },
-    {
-       nameSite: "bet365", 
-       info: {
-        bonusData: "",
-        bonusCode: "",
-        boostedOdds: "",
-        liveStreaming: "",
-        inPlayBetting: "",
-        termsCondidtions: "",
-       },
-       link: {
-        claimLink: "",
-        visitLink: "",
-        reviewLink: "",
-        },
-    },
-    {
-       nameSite: "William Hill", 
-       info: {
-        bonusData: "",
-        bonusCode: "",
-        boostedOdds: "",
-        liveStreaming: "",
-        inPlayBetting: "",
-        termsCondidtions: "",
-       },
-       link: {
-        claimLink: "",
-        visitLink: "",
-        reviewLink: "",
-        },
-    },
-    {
-       nameSite: "888 sport", 
-       info: {
-        bonusData: "",
-        bonusCode: "",
-        boostedOdds: "",
-        liveStreaming: "",
-        inPlayBetting: "",
-        termsCondidtions: "",
-       },
-       link: {
-        claimLink: "",
-        visitLink: "",
-        reviewLink: "",
-        },
-    },
-    {
-       nameSite: "name goes here", 
-       info: {
-        bonusData: "",
-        bonusCode: "",
-        boostedOdds: "",
-        liveStreaming: "",
-        inPlayBetting: "",
-        termsCondidtions: "",
-       },
-       link: {
-        claimLink: "",
-        visitLink: "",
-        reviewLink: "",
-        },
-    },
-]
+    //create a call back function
+    // Function to toggle the menu
+    const toggleMenu = () => {
+      setIsMenuOpen(!isMenuOpen);
+    };
+
+
     
- export const siteData2 = () => {
-    return (
-        <div className=''>
-        { siteData.list.map((nav, i) => (
-             <li key={i}>
-               <h1 className='li-head'>
-                 {nav.head}
-               </h1>
-               <p className="li-p1">
-                   {nav.paragraph}
-               </p>
-             </li>
-         ))
-       }
-       </div>
-    )
-}
+  return (
+    <div>
+
+    </div>
+  );
+};
+
+export default SitesData;
