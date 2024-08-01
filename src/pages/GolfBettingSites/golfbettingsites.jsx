03e655f6a3ce0a7cf64c065f33e16ca80b910b6f
MysteriousRob <robertjohngasior@gmail.com>
2024-08-01 14:25:03 +0200

    made the different sites

--- src/pages/GolfBettingSites/golfbettingsites.jsx
+++ src/pages/GolfBettingSites/golfbettingsites.jsx
@@ -1,20 +1,19 @@
 import React from "react";
 import Footer from "../../components/Footer/Footer";
 import Header from "../../components/Header/Header";
-import { useState } from "react";
 import SitesData from "../../state/sitesdata";
 // import williamHill from "../../assets/photos/williamHill.png";
 
 // follow same styling as cricket betting with the coloring being the same as the main sites
 const BoxComponent = () => {
-  
+
   return (
     <div>
     <div className="relative top-0 w-full">
       <Header />
     </div>
     <div>
-     
+      <SitesData />
     </div>
     <div className="relative bottom-0 w-full">
       <Footer />
